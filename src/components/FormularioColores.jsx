3ec46403c6aa7f033c559317707a03e11d8d63b1
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-27 12:08:02 -0300

    se corrige el title del form control

--- src/components/FormularioColores.jsx
+++ src/components/FormularioColores.jsx
@@ -6,13 +6,12 @@ const FormularioColores = () => {
     return(
     <section className="container" id="containerPrincipal">
         <h1 className="display-6 mb-0 text-center">Paleta de colores</h1>
-
         <Form className="d-flex flex-row justify-content-center mt-2">
         <Form.Control
         type="color"
         id="colorInput"
         defaultValue="#563d7c"
-        title="Choose your color"
+        title="Elegi un color"
         />
         <Form.Label className="pt-4 mt-4 ms-2" htmlFor="colorInput">Selecciona un color</Form.Label>
         <Button variant="primary" id="btnAgregar" type="submit" className="ms-3">Agregar</Button>
